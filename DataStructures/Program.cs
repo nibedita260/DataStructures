e3034cc3383db9c77006599eda6ac408ee5884a6
nibedita260 <deepi.nibedita260.com>
2021-10-04 20:22:18 +0530

    [Nibedita]:Added StackLinkedList Features Invented

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -6,21 +6,15 @@ namespace DataStructures
     {
         static void Main(string[] args)
         {
-            LinkedList linkedList = new LinkedList();
-            linkedList.Add(56);
-            linkedList.Add(30);
-            linkedList.Add(70);
-            //linkedList.AddReverseData(70);
-            //linkedList.AddReverseData(30);
-            //linkedList.AddReverseData(56);
-            //linkedList.InsertedAtPosition(30, 1);
-            //linkedList.RemoveFirstElem();
-            //linkedList.RemoveLastElem();
-            linkedList.InsertedAtPosition(40, 2);
-            //linkedList.Search(40);
-            //linkedList.RemoveFirstElem();
-            linkedList.Display();
-
+            LinkedListStack linkedListStack = new LinkedListStack();
+            linkedListStack.Push(56);
+            linkedListStack.Push(30);
+            linkedListStack.Push(70);
+            //linkedListStack.Display();
+            //linkedListStack.Peek();
+            //linkedListStack.Display();
+            linkedListStack.Pop();
+            linkedListStack.Display();
         }
     }
 }
