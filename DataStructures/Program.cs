809d9fffbd0ae4074bce5735fb185761dec54baf
nibedita260 <deepi.nibedita260.com>
2021-10-04 22:12:02 +0530

    [Nibedita]:Added StackLinkedList Features Invented

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -6,15 +6,45 @@ namespace DataStructures
     {
         static void Main(string[] args)
         {
-            LinkedListStack linkedListStack = new LinkedListStack();
-            linkedListStack.Push(56);
-            linkedListStack.Push(30);
-            linkedListStack.Push(70);
-            //linkedListStack.Display();
-            //linkedListStack.Peek();
-            //linkedListStack.Display();
-            linkedListStack.Pop();
-            linkedListStack.Display();
+            int options;
+            bool isExit = false;
+            while (!isExit)
+            {
+                Console.WriteLine("Choose 1:LinkedList 2:LinkedListStack");
+                options = Convert.ToInt32(Console.ReadLine());
+                switch (options)
+                {
+                    case 1:
+                        LinkedList linkedList = new LinkedList();
+                        linkedList.Add(56);
+                        linkedList.Add(30);
+                        linkedList.Add(70);
+                        linkedList.AddReverseData(70);
+                        linkedList.AddReverseData(30);
+                        linkedList.AddReverseData(56);
+                        linkedList.InsertedAtPosition(30, 1);
+                        linkedList.RemoveFirstElem();
+                        linkedList.RemoveLastElem();
+                        linkedList.InsertedAtPosition(40, 2);
+                        linkedList.Search(40);
+                        linkedList.RemoveFirstElem();
+                        linkedList.Display();
+                        break;
+                    case 2:
+                        LinkedListStack linkedListStack = new LinkedListStack();
+                        linkedListStack.Push(56);
+                        linkedListStack.Push(30);
+                        linkedListStack.Push(70);
+                        linkedListStack.Peek();
+                        linkedListStack.Pop();
+                        linkedListStack.IsEmpty();
+                        linkedListStack.Display();
+                        break;
+                    default:
+                        Console.WriteLine("choose valid one");
+                        break;
+                }
+            }
         }
     }
 }
