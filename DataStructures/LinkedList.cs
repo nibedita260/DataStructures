155d4fce2efc5ce377acc932329ac4c4d75af228
nibedita260 <deepi.nibedita260.com>
2021-10-04 11:21:24 +0530

    [Nibedita]:Added LinkedList Features Invented

---
+++ DataStructures/LinkedList.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures
+{
+    public class LinkedList
+    {
+        public Node head;
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.Next != null)
+                {
+                    temp = temp.Next;
+                }
+                temp.Next = node;
+            }
+            Console.WriteLine("{0} inserted into linkedList" ,node.data);
+        }
+        public void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("LinkedList is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.Next;
+            }
+        }
+        public void AddReverseData(int data)
+        {
+            Node newNode = new Node(data);
+            if (this.head == null)
+            {
+                this.head = newNode;
+            }
+            else
+            {
+                Node temp = this.head;
+                head = newNode;
+                head.Next = temp;
+            }
+        }
+        public void InsertedAtPosition(int data,int pos)
+        {
+            Node newNode = new Node(data);
+            if (this.head == null)
+            {
+                this.head= newNode; 
+            }
+            if (pos == 0)
+            {
+                newNode.Next = head;
+                this.head = newNode;
+            }
+            Node prev = null;
+            Node curr = this.head;
+            int count = 0;
+            while(curr!=null && count <pos)
+            {
+                prev = curr;
+                curr = curr.Next;
+                count++;
+            }
+            newNode.Next = prev.Next;
+            prev.Next = newNode;
+        }
+        public Node RemoveFirstElem()
+        {
+            if (this.head == null)
+            {
+                this.head = null;
+            }
+            this.head = this.head.Next;
+            return this.head;
+        }
+        public Node RemoveLastElem()
+        {
+            if (this.head == null)
+            {
+                this.head = null;
+            }
+            if (head.Next == null)
+            {
+                head.Next = null;
+            }
+            Node newNode = head;
+            while (newNode.Next.Next != null)
+            {
+                newNode = newNode.Next;
+            }
+            newNode.Next = null;
+            return head;
+        }
+        public Node Search(int value)
+        {
+            while (this.head != null)
+            {
+                if (this.head.data == value)
+                {
+                    return this.head;
+                }
+                this.head = this.head.Next;
+            }
+            return null;
+        }
+    }
+}
---
+++ DataStructures/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList linkedList = new LinkedList();
+            linkedList.Add(56);
+            linkedList.Add(30);
+            linkedList.Add(70);
+            //linkedList.AddReverseData(70);
+            //linkedList.AddReverseData(30);
+            //linkedList.AddReverseData(56);
+            //linkedList.InsertedAtPosition(30, 1);
+            //linkedList.RemoveFirstElem();
+            //linkedList.RemoveLastElem();
+            linkedList.InsertedAtPosition(40, 2);
+            //linkedList.Search(40);
+            //linkedList.RemoveFirstElem();
+            linkedList.Display();
+
+        }
+    }
+}
