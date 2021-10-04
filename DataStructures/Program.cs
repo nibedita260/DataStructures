0a4fc1ba8067d15b50ac07d0278b54346149f339
nibedita260 <deepi.nibedita260.com>
2021-10-04 22:09:55 +0530

    [Nibedita]:Added LinkedList Features Invented

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -6,20 +6,35 @@ namespace DataStructures
     {
         static void Main(string[] args)
         {
-            LinkedList linkedList = new LinkedList();
-            linkedList.Add(56);
-            linkedList.Add(30);
-            linkedList.Add(70);
-            //linkedList.AddReverseData(70);
-            //linkedList.AddReverseData(30);
-            //linkedList.AddReverseData(56);
-            //linkedList.InsertedAtPosition(30, 1);
-            //linkedList.RemoveFirstElem();
-            //linkedList.RemoveLastElem();
-            linkedList.InsertedAtPosition(40, 2);
-            //linkedList.Search(40);
-            //linkedList.RemoveFirstElem();
-            linkedList.Display();
+            int options;
+            bool isExit = false;
+            while (!isExit)
+            {
+                Console.WriteLine("Choose 1:LinkedList");
+                options = Convert.ToInt32(Console.ReadLine());
+                switch (options)
+                {
+                    case 1:
+                        LinkedList linkedList = new LinkedList();
+                        linkedList.Add(56);
+                        linkedList.Add(30);
+                        linkedList.Add(70);
+                        linkedList.AddReverseData(70);
+                        linkedList.AddReverseData(30);
+                        linkedList.AddReverseData(56);
+                        linkedList.InsertedAtPosition(30, 1);
+                        linkedList.RemoveFirstElem();
+                        linkedList.RemoveLastElem();
+                        linkedList.InsertedAtPosition(40, 2);
+                        linkedList.Search(40);
+                        linkedList.RemoveFirstElem();
+                        linkedList.Display();
+                        break;
+                    default:
+                        Console.WriteLine("choose valid one");
+                        break;
+                }
+            }
 
         }
     }
