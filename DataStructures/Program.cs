3940f6e45f17f53ade0a256b81399aedc0893887
nibedita260 <deepi.nibedita260.com>
2021-10-07 13:31:33 +0530

    [Nibedita]:Added LinkedList Generics Datastructure Problem Features Implemented

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -10,27 +10,27 @@ namespace DataStructures
             bool isExit = false;
             while (!isExit)
             {
-                Console.WriteLine("Choose 1:LinkedList");
+                Console.WriteLine("Choose 1:LinkedList" );
                 options = Convert.ToInt32(Console.ReadLine());
                 switch (options)
                 {
                     case 1:
-                        LinkedList linkedList = new LinkedList();
+                        LinkedList<int> linkedList = new LinkedList<int>();
                         linkedList.Add(56);
                         linkedList.Add(30);
                         linkedList.Add(70);
                         linkedList.AddReverseData(70);
                         linkedList.AddReverseData(30);
                         linkedList.AddReverseData(56);
-                        linkedList.InsertedAtPosition(30, 1);
+                        linkedList.InsertedAtPosition(40, 1);
                         linkedList.RemoveFirstElem();
                         linkedList.RemoveLastElem();
-                        linkedList.InsertedAtPosition(40, 2);
+                        linkedList.InsertedAtPosition(45, 2);
                         linkedList.Search(40);
-                        linkedList.RemoveFirstElem();
-                        linkedList.Display();
+                        //linkedList.RemoveFirstElem();
+                        //linkedList.Display();
                         linkedList.Size();
-                        linkedList.Sort();
+                        //linkedList.Sort();
                         linkedList.Display();
                         break;
                     default:
